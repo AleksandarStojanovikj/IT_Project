b6327a9312803640a4aca88ed170ae00876f6365
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2018-01-29 02:14:00 +0100

    Add 'Favorite' and 'add to watch' buttons

--- Mareandsangy/Home.aspx.cs
+++ Mareandsangy/Home.aspx.cs
@@ -10,13 +10,15 @@ using System.Web.UI.WebControls;
 public partial class Home : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
         if (!Page.IsPostBack) {
-            if(Session["username"] == null) {
+            loadContentOther();
+            loadContentRecommended();
+            if (Session["username"] == null) {
                 Response.Redirect("Login.aspx");
             }
             
         }
-        loadContentOther();
-        loadContentRecommended();
+        
+
 
     }
 
@@ -41,6 +43,7 @@ public partial class Home : System.Web.UI.Page {
                 movies[i].Poster = reader["Poster"].ToString();
                 movies[i].Plot = reader["Plot"].ToString();
                 movies[i].Year = " (" + reader["Year"].ToString() + ")";
+                movies[i].imdbID = reader["imdbID"].ToString();
                 i++;
                 if (i == 3)
                     break; 
@@ -59,6 +62,7 @@ public partial class Home : System.Web.UI.Page {
             lblYear1.Text= movies[0].Year;
             lblYear2.Text = movies[1].Year;
             lblYear3.Text = movies[2].Year;
+            ViewState["movieListR"] = movies;
 
         }
         catch (Exception err) {
@@ -77,7 +81,7 @@ public partial class Home : System.Web.UI.Page {
         SqlConnection connection = new SqlConnection();
         connection.ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
         // string sqlString = "Select Distinct Top 3 *  From Movies,Watched,Favorites WHERE Movies.imdbID=Watched.MovieID OR Movies.imdbID=Favorites.MovieID ORDER BY imdbRating";
-        string sqlString = "Select DISTINCT TOP 3 CHECKSUM(NEWID()),Watched.MovieID,Title,Year,Plot,Poster FROM Movies,Watched,Favorites WHERE Movies.imdbRating=Watched.MovieID OR Movies.imdbID=Favorites.MovieID ORDER BY CHECKSUM(NEWID())";
+        string sqlString = "Select DISTINCT TOP 3 CHECKSUM(NEWID()),Watched.MovieID,Title,Year,Plot,Poster,imdbID FROM Movies,Watched,Favorites WHERE Movies.imdbRating=Watched.MovieID OR Movies.imdbID=Favorites.MovieID ORDER BY CHECKSUM(NEWID())";
         SqlCommand command = new SqlCommand(sqlString, connection);
        
 
@@ -92,7 +96,8 @@ public partial class Home : System.Web.UI.Page {
                     movies[i].Poster = reader["Poster"].ToString();
                     movies[i].Plot = reader["Plot"].ToString();
                     movies[i].Year = " (" + reader["Year"].ToString() + ")";
-                    i++;
+                    movies[i].imdbID = reader["imdbID"].ToString();
+                i++;
                 
                 if (i == 3)
                     break;
@@ -111,6 +116,7 @@ public partial class Home : System.Web.UI.Page {
             lblYear11.Text = movies[0].Year;
             lblYear22.Text = movies[1].Year;
             lblYear33.Text = movies[2].Year;
+            ViewState["movieListO"] = movies;
 
         }
         catch (Exception err) {
@@ -119,5 +125,105 @@ public partial class Home : System.Web.UI.Page {
         finally {
             connection.Close();
         }
+    }
+
+    protected void addToFavorites(string id) {
+        SqlConnection connection = new SqlConnection();
+        connection.ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+        string sqlString = "INSERT INTO Favorites (MovieID, Username) values (@movieID, @username)";
+        SqlCommand command = new SqlCommand(sqlString, connection);
+        command.Parameters.AddWithValue("@movieID", id);
+        command.Parameters.AddWithValue("@username", Session["username"].ToString());
+
+        try {
+            connection.Open();
+            command.ExecuteNonQuery();
+        }catch(Exception err) {
+            lblError.Text = err.Message;
+        }
+        finally {
+            connection.Close();
+        }
+    }
+
+    protected void addToWatch(string id) {
+        SqlConnection connection = new SqlConnection();
+        connection.ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+        string sqlString = "INSERT INTO Watched (MovieID, Username, watched) values (@movieID, @username, @watched)";
+        SqlCommand command = new SqlCommand(sqlString, connection);
+        command.Parameters.AddWithValue("@movieID", id);
+        command.Parameters.AddWithValue("@username", Session["username"].ToString());
+        command.Parameters.AddWithValue("@watched", "false");
+
+        try {
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+        catch (Exception err) {
+            lblError.Text = err.Message;
+        }
+        finally {
+            connection.Close();
+        }
+    }
+
+    protected void btnFav1_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToFavorites(movies[0].imdbID.ToString());
+    }
+
+    protected void btnFav2_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToFavorites(movies[1].imdbID.ToString());
+    }
+
+    protected void btnFav3_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToFavorites(movies[2].imdbID.ToString());
+    }
+
+    protected void btnFav4_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToFavorites(movies[0].imdbID.ToString());
+    }
+
+    protected void btnFav5_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToFavorites(movies[1].imdbID.ToString());
+    }
+
+    protected void btnFav6_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToFavorites(movies[2].imdbID.ToString());
+    }
+
+    protected void btnWatch1_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToWatch(movies[0].imdbID.ToString());
+    }
+
+    protected void btnWatch2_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToWatch(movies[1].imdbID.ToString());
+    }
+
+    protected void btnWatch3_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListR"] as List<Movie>;
+        addToWatch(movies[2].imdbID.ToString());
+    }
+
+    protected void btnWatch4_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToWatch(movies[0].imdbID.ToString());
+    }
+
+    protected void btnWatch5_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToWatch(movies[1].imdbID.ToString());
+    }
+
+    protected void btnWatch6_Click(object sender, EventArgs e) {
+        List<Movie> movies = ViewState["movieListO"] as List<Movie>;
+        addToWatch(movies[2].imdbID.ToString());
     }
 }
